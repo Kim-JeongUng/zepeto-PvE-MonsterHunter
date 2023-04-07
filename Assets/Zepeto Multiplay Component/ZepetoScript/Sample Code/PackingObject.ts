e1a783929e3be8eca08caebf5acda851f3f10276
Kim JeongUng <82865325+Kim-JeongUng@users.noreply.github.com>
2023-04-07 16:40:57 +0900

    update : multiplay module

--- Assets/Zepeto Multiplay Component/ZepetoScript/Sample Code/PackingObject.ts
+++ Assets/Zepeto Multiplay Component/ZepetoScript/Sample Code/PackingObject.ts
@@ -1,5 +1,5 @@
 import { ZepetoScriptBehaviour } from 'ZEPETO.Script'
-import {WaitUntil, HumanBodyBones, Vector3, Quaternion, Object} from "UnityEngine";
+import {WaitUntil, HumanBodyBones, Vector3, Quaternion, Object, WaitForEndOfFrame} from "UnityEngine";
 import {ZepetoPlayers} from "ZEPETO.Character.Controller";
 import TransformSyncHelper from '../Transform/TransformSyncHelper';
 import MultiplayManager from '../Common/MultiplayManager';
@@ -8,18 +8,20 @@ export default class PackingObject extends ZepetoScriptBehaviour {
     // This is a script that makes the Zepeto player move together by setting the object as a child object.
     // For example, holding a gun, holding a balloon,
     @SerializeField() private targetBone: HumanBodyBones;
-    @SerializeField() private localPosition: Vector3;
-    @SerializeField() private localRotation: Vector3;
+    @SerializeField() private localPosition: Vector3 = Vector3.zero;
+    @SerializeField() private localRotation: Vector3 = Vector3.zero;
     private _tfHelper:TransformSyncHelper;    
 
     private Start() {    
         this._tfHelper = this.transform.GetComponent<TransformSyncHelper>();
-        this.StartCoroutine(this.MountingObject(this._tfHelper.OwnerSessionId));
+        if(this._tfHelper.OwnerSessionId)
+            this.StartCoroutine(this.MountingObject(this._tfHelper.OwnerSessionId));
     }
 
     private *MountingObject(ownerSessiondId :string){
-        console.log(ownerSessiondId);
-        const user = MultiplayManager.instance.room.State.players?.get_Item(ownerSessiondId);
+        yield new WaitForEndOfFrame();
+        
+        const user = MultiplayManager.instance?.room?.State?.players?.get_Item(ownerSessiondId) ?? null;
         if(user !== null) {
             yield new WaitUntil(() => ZepetoPlayers.instance.HasPlayer(ownerSessiondId));
             const player = ZepetoPlayers.instance.GetPlayer(ownerSessiondId).character;
