38b3b19c499fb98a62006b0d8efea0e35c8cd4d5
Kim JeongUng <82865325+Kim-JeongUng@users.noreply.github.com>
2023-04-29 03:57:09 +0900

    feat : server monster interface

--- Assets/Server/World.multiplay/ServerModule/Modules/MonsterHunterModule.ts
+++ Assets/Server/World.multiplay/ServerModule/Modules/MonsterHunterModule.ts
@@ -6,8 +6,30 @@ import { loadDataStorage} from 'ZEPETO.Multiplay.DataStorage'
 const MaxExp = 30;
 
 export default class MonsterHunterModule extends IModule {
+    private monsterData: Record<string, IMonsterData> = {
+        "Golem": {
+            Name: "Golem",
+            MaxHp: 100,
+            Power: 10,
+            RewardCurrency: 10,
+            RewardExp: 10,
+        },
+        "Slime": {
+            Name: "Slime",
+            MaxHp: 500,
+            Power: 20,
+            RewardCurrency: 20,
+            RewardExp: 20,
+        },
+        // ...
+    };
+    private serverObjId = 10000;
+
     async OnCreate() {
         /**Monster Sync**/
+        this.CreateBaseMonster();
+
+        //TODO : 서버에서 Set해서 클라로 주는걸로 변경
         this.server.onMessage(MESSAGE.SetEntity, (client, message) => {
             const { ObjectId, isMonster, MaxHp, Hp } = message;
 
@@ -82,17 +104,33 @@ export default class MonsterHunterModule extends IModule {
     OnTick(deltaTime: number) {
 
     }
+    CreateBaseMonster(){
+        const monster = this.monsterData['Golem'];
+
+        this.server.broadcast(MESSAGE.SpawnMonster, monster);
+        entity = new GameEntity();
+        entity.ObjectId = this.serverObjId++;
+        entity.Name = monster.Name;
+        entity.isMonster = true;
+        entity.MaxHp = monster.MaxHp;
+        entity.Hp = monster.MaxHp;
+
+        this.server.state.GameEntities.set(ObjectId.toString(), entity);
+    }
 
     DeathEvent(attacker:string, victim:string){
-        this.server.broadcast("DeathEvent"+victim,attacker);
-        this.server.broadcast("KillEvent"+attacker,victim);
-        const monsterReward: MonsterReward = {
-            Currency : 10,
-            Exp : 10
+        const entity = this.server.state.GameEntities.get(victim);
+        if(entity !== null){
+            const monster = this.monsterData[entity.name];
+
+            const monsterReward: MonsterReward = {
+                Currency : monster.RewardCurrency,
+                Exp : monster.RewardExp
+            }
+            this.server.broadcast("OnReward"+attacker,monsterReward);
         }
-        this.server.broadcast("OnReward"+attacker,monsterReward);
-        console.log("log 전송");
     }
+
     async GetAllPlayerData(client : SandboxPlayer){
         let isNewMember : boolean = false;
         const defaultValues: [string, number][] = [
@@ -130,9 +168,13 @@ interface IPlayerData {
     [key: string]: number;
 }
 
-interface MonsterReward{
-    Currency: number;
-    Exp: number;
+interface IMonsterData {
+    Name : string;
+    MaxHp : number;
+    Power : number;
+    //reward
+    RewardCurrency: number;
+    RewardExp: number;
 }
 
 enum MESSAGE {
@@ -140,5 +182,6 @@ enum MESSAGE {
     TakeDamage = "TakeDamage",
     GainHp = "GainHp",
     GainExp = "GainExp",
-    GetAllPlayerData = "GetAllPlayerData"
+    GetAllPlayerData = "GetAllPlayerData",
+    SpawnMonster = "SpawnMonster"
 }
