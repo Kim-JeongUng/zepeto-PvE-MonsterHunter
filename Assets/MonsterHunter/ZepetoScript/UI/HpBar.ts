64c1181288c792a1b8801e82a0b2de4fea6360ef
Kim JeongUng <82865325+Kim-JeongUng@users.noreply.github.com>
2023-05-10 03:47:09 +0900

    feat : HpBar

--- Assets/MonsterHunter/ZepetoScript/UI/HpBar.ts
+++ Assets/MonsterHunter/ZepetoScript/UI/HpBar.ts
@@ -1,37 +1,47 @@
 import { ZepetoScriptBehaviour } from 'ZEPETO.Script'
-import { Image } from 'UnityEngine.UI';
-import { Time, Mathf } from 'UnityEngine';
+import { ZepetoCamera, ZepetoPlayers } from 'ZEPETO.Character.Controller'
+import { Slider } from 'UnityEngine.UI';
+import { Time, Mathf, Vector3, GameObject } from 'UnityEngine';
 
 export default class HPBar extends ZepetoScriptBehaviour {
-    @SerializeField() private _fillImage: Image;
-    @SerializeField() private _maxHP: number = 100;
+    @SerializeField() private HpSlider: Slider;
+    @SerializeField() private hpFillImage: GameObject;
+    
+    private _zepetoCamera : ZepetoCamera;
+    private _maxHP: number = 100;
     private _currentHP: number = 100;
     private _targetHP: number = 100;
     private _lerpSpeed: number = 5.0;
     private _isDamaged: boolean = false;
 
     Start() {
-        this.UpdateHPBar();
+        this._zepetoCamera = ZepetoPlayers.instance.ZepetoCamera;
     }
 
     Update() {
-        if (this._isDamaged) {
-            this._currentHP = Mathf.Lerp(this._currentHP, this._targetHP, Time.deltaTime * this._lerpSpeed);
-            this.UpdateHPBar();
-            if (Mathf.Abs(this._currentHP - this._targetHP) < 0.1) {
-                this._isDamaged = false;
-            }
-        }
+        
+        this.UpdateHpBarRotation();
+        
+        // if (this._isDamaged) {
+        //     this._currentHP = Mathf.Lerp(this._currentHP, this._targetHP, Time.deltaTime * this._lerpSpeed);
+        //     if (Mathf.Abs(this._currentHP - this._targetHP) < 0.1) {
+        //         this._isDamaged = false;
+        //     }
+        // }
     }
 
     TakeDamage(amount: number) {
         this._isDamaged = true;
         this._targetHP = Mathf.Clamp(this._targetHP - amount, 0, this._maxHP);
     }
+    
+    public SetHpBar(value:number){
+        this.HpSlider.value = value;
+        this.hpFillImage.SetActive(value > 0);
+    }
 
-    UpdateHPBar() {
-        let fillAmount: number = this._currentHP / this._maxHP;
-        this._fillImage.fillAmount = fillAmount;
+    private UpdateHpBarRotation() {
+        this.transform.LookAt(this._zepetoCamera.transform);
     }
 }
 
